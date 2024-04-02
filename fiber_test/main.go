6f88b4725b849006388c859bdc58ce03f1f323ad
Chakrit <chakrit.t@arise.tech>
2024-04-02 23:58:30 +0700

    fiber test

---
+++ fiber_test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting sever at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
